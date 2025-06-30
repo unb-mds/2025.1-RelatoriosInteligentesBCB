fb04e0890b113f5f91e8ee360be90ad46753352a
Gabriel Guedes Fernandes <gabrielguedesf1520@gmail.com>
2025-06-30 11:10:12 -0300

    feat: "Importando plotly.graph_objects"

--- app_pages/Previsoes_ML.py
+++ app_pages/Previsoes_ML.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 import time
 from components.indicadores import indicator_names, load_data
